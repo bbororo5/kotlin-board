3a0fef9ddfe790b18b93461e7420024daeb50819
bbororo5 <114351464+bbororo5@users.noreply.github.com>
2025-03-11 00:00:15 +0900

    Initial commit

---
+++ src/main/kotlin/org/example/ktpost/KtPostApplication.kt
@@ -0,0 +1,11 @@
+package org.example.ktpost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtPostApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtPostApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/ktpost/KtPostApplicationTests.kt
@@ -0,0 +1,12 @@
+package org.example.ktpost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtPostApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
